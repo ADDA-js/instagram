bb3a7646f436ddda686c5a6f88417a16e13a67ec
adam driggers <awdriggs@gmail.com>
2016-11-18 19:43:58 -0500

    testing the ajax call

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
+//things I had to do first...
+// I created three files, 
+// - a index.html with a login button
+// - a app.js
+// - a auth.html, this is the redirect page i will use later
+//
+// I setup a github pages to host this site, directions here https://pages.github.com/
+// I registered a client https://www.instagram.com/developer/clients/manage/
+// - use the github pages url as the project uri
+// - use the github pages url/auth.html as the redirect uri
+// - under security tab, unclick the "Disable explicit OAuth"
+// I used the the explicit auth link from the instagram developers site
+// with my client id and the redirect site
+
 console.log('working yo');
 
-var token = window.location.href.split('=')[1];
-console.log(token);
+$(document).ready(function() {
+
+  //this will get the url and split it at the =, the second item is the token
+  var token = window.location.href.split('=')[1];
+  //the token is what we need for the api
+
+  $.ajax({
+    url: 'https://api.instagram.com/v1/tags/search?q=snowy&access_token='+token,
+    method: 'GET';
+  }).done();
+});
+
+var return = function(data){
+  debugger;
+}
